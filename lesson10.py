44358bdfc4a2951e0614adbcd48870dd4da79e86
Jeff Bergcoutts <bergcoutts@spotify.com>
2019-02-03 14:37:25 +0100

    lesson10 working

--- lesson10.py
+++ lesson10.py
@@ -1,29 +1,56 @@
+'''
+Solving Problems notes
+0. Relax, don't panic
+1: What are the inputs?
+- what is the set of valid inputs
+- check for invalid
+- you decide how inputs are represented, strings, object
+2: What are the outputs?
+3. Understand the relationship between inputs and outputs - Work out some examples
+
+Try to solve the problem without code
+Then try to write an algorithm that solves it - can use pseudo code
+4. Simple mechanical solution - don't optimize prematurely - make sure it will work
+
+Making progress - write small bits of code, test them, know what they do
+'''
+
 days_of_months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def is_leap_year(year):
     return False
 
-def days_between_dates(y1, m1, d1, y2, m2, d2):
-    if y2 < y1:
-        return "dates not valid"
-    elif y2 == y1:
-        if m2 < m1:
-            return "dates not valid"
-        elif m2 == m1:
-            if d2 < d1:
-                return "dates not valid"
-            else:
-                return d2 - d1
-        elif m2 > m1:
-            # add up all the days between the 2 months
-            days_in_first_month = days_of_months[m1 - 1] - d1
-            return days_in_first_month + sum(days_of_months[m1:m2-1]) + d2
-    elif y2 > y1:
-        days_in_first_month = days_of_months[m1 - 1] - d1
-        rest_of_days_in_year = sum(days_of_months[m1:])
-        days_in_years_inbetween = sum(days_of_months) * ((y2-1)-y1)
-        days_in_final_year = sum(days_of_months[:m2-1])
-        return days_in_first_month + rest_of_days_in_year + days_in_years_inbetween + days_in_final_year + d2
+def nextDay(y,m,d):
+    """warning this assumes all months have 30 days"""
+    if d < 30:
+        return y, m, d + 1
+    else:
+        if m < 12:
+            return y, m + 1, 1
+        else:
+            return y + 1, 1, 1
 
+def isLessThenDate(y, m, d, y2, m2, d2):
+    if y < y2:
+        return True
+    elif m < m2:
+        return True
+    elif d < d2:
+        return True
+    else:
+        return False
 
-print days_between_dates(2012, 2, 15, 2019, 2, 18)
+def daysBetweenDates(y1, m1, d1, y2, m2, d2):
+    noOfDays = 0
+    y, m, d = y1, m1, d1
+    while isLessThenDate(y, m , d, y2, m2, d2):
+        noOfDays = noOfDays + 1
+        y, m, d = nextDay(y, m, d)
+    
+    return noOfDays
+
+print daysBetweenDates(2012, 2, 15, 2019, 2, 18)
+print daysBetweenDates(2018,4,30,2019,3,12)
+print daysBetweenDates(2018,4,21,2018,4,22)
+print daysBetweenDates(2018,4,30,2018,6,12)
+print daysBetweenDates(2018,4,39,2018,4,30)
