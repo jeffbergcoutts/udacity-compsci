54551aa7499aefc170ea09902f6056fe2f57abfa
Jeff Bergcoutts <jeff.bergcoutts@gmail.com>
2019-02-23 14:02:27 +0100

    lesson 11 working

--- lesson11.py
+++ lesson11.py
@@ -33,7 +33,7 @@ print stooges
 
 # Yello Mutation
 '''
-Lists are mutatable. Changing the value of a list 
+Lists are mutatable. Changing the value of a list
 effect any variable with that list. eg:
 '''
 p = ['H', 'E', 'L', 'L', 'O']
@@ -48,3 +48,26 @@ print spy, agent
 print agent[2]
 spy[2] = agent[2] + 1
 print agent[2]
+
+'''
+13: Quiz: Replace spy
+'''
+print "13: Quiz"
+spy = [0,0,7]
+def replace_spy(p):
+    p[2] = p[2] + 1
+
+replace_spy(spy)
+print spy
+
+# p[2] = p[2] + 1 <-- this is a replacement statement, also changes spy
+# a = a + 1 <-- this is an assignment statement, would not change spy if spy was "a"
+
+print "17: Quiz"
+p = [1,2]
+q = [3,4]
+p.append(q)
+print p
+q[1] = 5
+print p
+# this will change the list p as well because q is in p, we appened a reference to q
---
+++ python_cheatsheet.md
@@ -0,0 +1,25 @@
+Add to the end of a list
+```
+<list>.append(<element>)
+```
+
+Add to lists together
+```
+<list> + <list>
+eg. [0,1] + [2,3] = [0,1,2,3]
+```
+
+Get length of list
+```
+len(<list>)
+```
+
+## Memory
+
+* fastest Memory is on the Register - temporary, built into processor, about a KB or 2 of memory
+* next fastest is DRAM, on the Capacitors
+* both store electrons
+2 GB of RAM = 2**30 * 2 bytes
+* 8 = 17 Billion bits (1 or 0)
+latency of DRAM is 12 nanoseconds
+* Hard drive stores magnetic - with a read and write arm - slow
